1a7abf2fa850b1b5972ee3bed261ea32c88fb1a3
Ramtin <ramtinsharif@gmail.com>
2018-12-28 23:21:41 +0330

    add close modal to see product NavLink

--- src/reducers/modalReducer.js
+++ src/reducers/modalReducer.js
@@ -4,12 +4,15 @@ const modalReducer=(state=initialState,action)=>{
 
     switch(action.type){
         case 'MODAL_CLICK':
-            const product=action.product
+            const product=action.product;
             const newProduct=Object.assign(product, {mode: true});
             return newProduct;
             
         case 'CLOSE_MODAL':
             return !state;
+            
+        case 'ADD_ITEM_FROM_MODAL':
+            return {mode:!state}
 
         default:
             return state;
