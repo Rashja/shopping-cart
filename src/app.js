7f0ec91d48e785ed24b16aa09b076200c7655428
Ramtin <ramtinsharif@gmail.com>
2019-01-03 17:45:52 +0330

    install persist and config it

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import "regenerator-runtime/runtime";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './reducers/index';
+import { PersistGate } from 'redux-persist/integration/react'
+import {store,persistor} from './reducers/index';
 import MainRoute from './routers/MainRoute';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/style.css";
@@ -10,7 +11,9 @@ import "./style/style.css";
 
 const Jsx=(
     <Provider store={store} >
-        <MainRoute />
+        <PersistGate loading={null} persistor={persistor} >
+            <MainRoute />
+        </PersistGate>
     </Provider>
 )
 ReactDOM.render(Jsx,document.getElementById('app'))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,27 @@
 import { createStore,applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootSaga from './../sagas/sagas';
 import reducers from './combiner';
 
 const sagaMiddleware=createSagaMiddleware();
 
+const persistConfig={
+    key:'app',
+    storage
+}
+const persistedReducer=persistReducer(persistConfig,reducers)
+
 const store=createStore(
-    reducers,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+const persistor=persistStore(store)
+
+
+export {store,persistor};
